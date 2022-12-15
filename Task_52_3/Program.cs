6ad148a46251ca26872f088a3ed3eba11a3125ae
Sanders7676 <118454007+Sanders7676@users.noreply.github.com>
2022-12-15 13:47:36 +0300

    Update Program.cs
    Made changes to code of Task_52_3

--- Task_52_3/Program.cs
+++ Task_52_3/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] array = CreateRandom2DArray(3, 4);
+
+
+
+
+
+
+// Метод генерации массива из случайных чисел
+
+int[,] CreateRandom2DArray(int countOfRows, int countOfColumns)
+{
+    int[,] arr = new int[countOfRows, countOfColumns];
+
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            arr[i, j] = random.Next(-10, 11);
+        }
+    }
+    return arr;
+}
+
+
+
+// Метод нахождения сумм ячеек по столбцам в столбце
+
+int GetColumnSums (int[,] array)
+{
+    int sum = 0;
+      
+    int[] columnSums = new int[array.GetLength(1)];
+    
+    
+  
+    for (var j = 0; j < array.GetLength(1); j++)
+    {
+        for (var i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+            
+        }
+        
+      
+    }
+    return arr; 
+    
+    
+// Метод нахождения среднего арифметического в столбце
+
+int GetArithmeticalMean (int[,] array)
+{
+    int sum = 0;
+      
+    int arithmeticalMean = 0;
+      
+    int[] arithmeticalMean = new int[array.GetLength(1)];
+    
+    for (var j = 0; j < array.GetLength(1); j++)
+    {
+        for (var i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];                     
+        }
+        arithmeticalMean = sum / GetLength(0)
+        
+        arithmeticalMean [i, j];
+      
+    }
+    return arr; 
